615b2c27bace46d4747a354907f89a8b8b6de80d
Lindsey Rampton <lrampton@zingermans.com>
2024-07-09 11:22:08 -0400

    :sparkles: initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    //get media stream
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    //pass to video element
+    videoElement.srcObject = mediaStream;
+    //play video
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+
+  } catch (error) {
+    // Catch Error Here
+    console.log('Whoops, error here:', error);
+  } 
+}
+
+button.addEventListener('click', async () => {
+  // Disable Button
+  button.disabled = true;
+  // Start Picture in Picture
+  await videoElement.requestPictureInPicture();
+  // Reset Button
+  button.disabled = false;
+});
+
+//on load 
+selectMediaStream();
